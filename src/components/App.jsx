e69b25b71c2f8a2b53193719819bd5bfa74c743f
ProskurinV <proskyrinv.g@gmail.com>
2022-09-21 01:13:08 +0300

    kd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
+import Painting from './components/Painting';
+import paintings from '../../src/';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Painting
+        imageUrl={paintings[0].url}
+        title={paintings[0].title}
+        author={paintings[0].author.tag}
+        profileUrl={paintings[0].author.url}
+        price={paintings[0].price}
+      />
+      <Painting
+        imageUrl={paintings[1].url}
+        title={paintings[1].title}
+        author={paintings[1].author.tag}
+        profileUrl={paintings[1].author.url}
+        price={paintings[1].price}
+      />
+      <Painting
+        imageUrl={paintings[2].url}
+        title={paintings[2].title}
+        author={paintings[2].author.tag}
+        profileUrl={paintings[2].author.url}
+        price={paintings[2].price}
+      />
     </div>
   );
-};
+}
---
+++ src/components/Painting.js
@@ -0,0 +1,24 @@
+import defaultImage from './default.jpg';
+
+const Painting = ({
+  imageUrl = defaultImage,
+  title,
+  profileUrl,
+  author = 'не известно',
+  price,
+}) => {
+  return (
+    <div>
+      <img src={imageUrl} alt={title} width="480" />
+      <h2>{title}</h2>
+      <p>
+        Автор: <a href={profileUrl}>{author}</a>
+      </p>
+      <p>Цена: {price} кредитов</p>
+      <p>Доступность: </p>
+      <button type="button">Добавить в корзину</button>
+    </div>
+  );
+};
+
+export default Painting;
