a41d599fcad16c2fb12b34a7dc22bb363997951e
ProskurinV <proskyrinv.g@gmail.com>
2022-09-29 23:22:49 +0300

    +

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ import ColorPicker from 'components/ColorPicker';
 import Form from 'components/Form';
 import { Component } from 'react';
 
-// const colorPickerOptions = [
-//   { label: 'red', color: '#F44336' },
-//   { label: 'green', color: '#4CAF50' },
-//   { label: 'blue', color: '#2196F3' },
-//   { label: 'grey', color: '#607D8B' },
-//   { label: 'pink', color: '#E91E63' },
-//   { label: 'indigo', color: '#3F51B5' },
-// ];
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
 
 export default class App extends Component {
   formHandlerSubmit = data => {
@@ -26,14 +26,14 @@ export default class App extends Component {
     return (
       <Box bg="mute" color="text">
         <div>
-          {/* <Section title="top of week">
-          <PaintingList paintings={paintings} />
-          <PaintingList paintings={paintings} />
-        </Section>
-        <Section />
-        <Counter />
-        <Dropdown />
-        <ColorPicker options={colorPickerOptions} /> */}
+          <Section title="top of week">
+            <PaintingList paintings={paintings} />
+            <PaintingList paintings={paintings} />
+          </Section>
+          <Section />
+          <Counter />
+          <Dropdown />
+          <ColorPicker options={colorPickerOptions} />
           <Form onSubmit={this.formHandlerSubmit} />
         </div>
       </Box>
